617cd06b7156faed8e8483a23e2a32db6247be9c
PeterLinLei <84164000+PeterLinLei@users.noreply.github.com>
2022-08-10 17:35:47 +0800

    Add files via upload

--- FlexLicenseLog.sh
+++ FlexLicenseLog.sh
@@ -3,3 +3,4 @@ cd /opt/tomcat/java/FlexLicenseLog/
 java -jar /opt/tomcat/java/FlexLicenseLog/FlexLicenseLog.jar /opt/tomcat/java/FlexLicenseLog/ /share/apps/ansys_inc/shared_files/licensing/license.log 0 > /opt/tomcat/java/FlexLicenseLog/ansysLS.out &
 # java -jar /opt/tomcat/java/FlexLicenseLog/FlexLicenseLog.jar /opt/tomcat/java/FlexLicenseLog/ /share/apps/Fluent.Inc/FSLM10.8/lnamd64/license.log 0 > /opt/tomcat/java/FlexLicenseLog/fluentLS.out &
 
+
